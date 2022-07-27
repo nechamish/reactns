91b7c5d0f579d8acbd687cef370c1c606b558d55
shoshizada <s0556737338@gmail.comgit clone https://github.com/shoshizada/WeightProject.git>
2022-07-27 14:50:58 +0300

    add

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -1,48 +1,51 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+// import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-import useNavigate from "use-navigate";
+// import useNavigate from "use-navigate";
 export default function Signiin() {
 
-    let navigate = useNavigate();
+    // let navigate = useNavigate();
 const [password, setPassword] = useState();
- const sendToDb = async () => {
+ const sendToDb = async  => {
     console.log(password);
-    const promise = axios.post(
-      "https://meetings-test.herokuapp.com/user/signin",
-      {
-        Password: password,
-      }
-    );
-    const res = await promise;
-    console.log(res.data);
-    debugger;
-    return res.data;
+    console.log(parseInt(password));
+    if(parseInt(password)===100){
+      console.log("i am manager");
+    }
+    else{
+         try {
+       const res = axios.get("https://meetings-test.herokuapp.com/user/signin")
+         .then(() => {
+         business = res.data;
+         debugger;
+            });
+          } catch (err) {  console.log(err);}
+    }
   };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    if (parseInt(data.get("password")) === 100) {
-      console.log("manager");
-      return navigate("/Manager");
-    }
-    //אם הוא עובד קיים
-    const user = await sendToDb();
-    // console.log(user);
-    if (user.userid) {
-      console.log("i am user");
-      navigate("/User", { state: { user } });
-    }
-   }
+  // const handleSubmit = async (event) => {
+  //   // event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   if (parseInt(data.get("password")) === 100) {
+  //     console.log("manager");
+  //     // return navigate("/Manager");
+  //   }
+  //   //אם הוא עובד קיים
+  //   const user = await sendToDb();
+  //   // console.log(user);
+  //   if (user.userid) {
+  //     console.log("i am user");
+  //     // navigate("/User", { state: { user } });
+  //   }
+  //  }
 
   return (
     <>
       <p>enter username</p>
       <input></input>
       <p>enter password</p>
-      <input id="password" name="password" type="password"></input>
-      <button  onclick={handleSubmit()}>sginin</button>
+      <input onChange={e=>{setPassword(e.target.value)}} id="password" name="password" type="password"></input>
+      <button onSubmit={sendToDb()}>sginin</button>
     </>
   );
 }
